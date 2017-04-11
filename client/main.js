ab3f6f84cfaa7160c429a8a2f599f5236873f710
Laurent botella <agence@paus-web.fr>
2017-04-11 22:50:54 +0200

    premier routing

--- client/main.js
+++ client/main.js
@@ -3,5 +3,5 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
-// on souscrit a la publications des posts pour les afficher cote client
+// on souscrit a la publications des posts de la collection pour les afficher cote client
 Meteor.subscribe('posts');
