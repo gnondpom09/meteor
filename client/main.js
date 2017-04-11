ff87d2955e10163e9648e867ded8233a96b5cc8a
Laurent botella <agence@paus-web.fr>
2017-04-11 23:51:41 +0200

    ajout routing complet avec page non trouve

--- client/main.js
+++
@@ -1,7 +0,0 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
-
-import './main.html';
-
-// on souscrit a la publications des posts de la collection pour les afficher cote client
-Meteor.subscribe('posts');
