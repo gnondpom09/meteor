e87feb12608961041ce4ca2051e0b7cf536a698e
Laurent botella <agence@paus-web.fr>
2017-04-16 12:33:13 +0200

    ajout collection notifications

--- server/publications.js
+++ server/publications.js
@@ -1,14 +1,21 @@
 /**
  * publications dans la collection Mongo db cote serveur
  */
- // affiche les posts
+
+ // publie les posts
  Meteor.publish('posts', function() {
    return Posts.find();
  });
 
+// publie les commentaires
  Meteor.publish('comments', function(postId) {
    check(postId, String);
    return Comments.find({
      postId: postId
    });
  });
+
+// publie les notifications
+ Meteor.publish('notifications', function() {
+   return Notifications.find();
+ })
